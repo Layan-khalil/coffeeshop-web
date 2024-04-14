5236f419c9e02e4d36f414596ce1ee5ee5999fd9
codingwith.layan <layan.michael1@gmail.com>
2024-04-13 22:05:55 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,28 @@
+var navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    
+    searchform.classList.remove('active');
+    cartitem.classList.remove('active');
+}
+
+var cartitem = document.querySelector('.cart-item-container');
+document.querySelector('#cart-btn').onclick = () => {
+    cartitem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchform.classList.remove('active');
+    
+}
+
+var searchform = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () => {
+    searchform.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartitem.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchform.classList.remove('active');
+    cartitem.classList.remove('active');
+}
